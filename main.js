ba7a132da6e7f670521d6667a2e1eb7d18d40205
Rob Christian <rm.rf.etc@gmail.com>
2016-10-17 01:17:21 -0700

    Update for 2016

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
 module.exports = MVC
-MVC.settings = require('e_settings')
+MVC.settings = require('./internal/e_settings.json')
 function MVC(app){
     var NODE_DOMAIN, fs, _
     // NODE_DOMAIN = require('domain').create()
     // NODE_DOMAIN.on('error', function (err) { console.log(err.message, err.stack) })
     // NODE_DOMAIN.run(function(){
 
-    _ = require('lodash-node')
+    _ = require('lodash')
     _.chokidar = require('chokidar')
     fs = require('fs')
 
-    _.extend(_, require('e_helpers_io'))
-    _.extend(_, require('e_helpers_path'))
-    _.extend(_, require('e_helpers_process'))
-    _.extend(_, require('e_helpers_typecheck'))
+    _.extend(_, require('./internal/e_helpers_io.js'))
+    _.extend(_, require('./internal/e_helpers_path.js'))
+    _.extend(_, require('./internal/e_helpers_process.js'))
+    _.extend(_, require('./internal/e_helpers_typecheck.js'))
     _.log = _.noop
 
     _.watch = function(file, cb){
@@ -45,15 +45,20 @@ function MVC(app){
         return require('https').createServer(cert, router.listener)
     }
 
-    easyioc
-        .add( '_',               adapt(_)               )
-        .add( 'router',          adapt(router)          )
-        .add( 'ioc',             adapt(easyioc)         )
-        .add( 'fetchfiles',      adapt('filefetcher')   )
-        .add( 'RESTController',  adapt('e_controllers') )
-        .add( 'views',           'e_views'              )
-        .add( 'settings',        MVC.settings           )
-        .add( 'server',          server                 )
-        .add( 'main',            app                    )
-        .exec()
+    var dependencies = {
+        '_':              adapt(_),
+        'router':         adapt(router),
+        'ioc':            adapt(easyioc),
+        'fetchfiles':     adapt('filefetcher'),
+        'RESTController': adapt('./internal/e_controllers.js'),
+        'views':          require('./internal/e_views.js'),
+        'settings':       MVC.settings,
+        'server':         server,
+        'main':           app,
+    }
+
+    Object.keys(dependencies).forEach(function(key){
+        easyioc.add(key, dependencies[key])
+    })
+    easyioc.exec()
 }
